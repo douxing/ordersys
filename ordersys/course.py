9271ab11c9a57661d8f2f178c19503633afb293a
dx <douxing1983@outlook.com>
2018-05-21 00:49:14 +0800

    add create

--- ordersys/course.py
+++ ordersys/course.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -22,3 +24,40 @@ def index():
     ).fetchall()
 
     return render_template('course/index.html', courses=courses)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        now = datetime.now()
+
+        title = request.form['title']
+        description = request.form['description']
+        image_url = request.form['image_url']
+        price = request.form['price']
+        quantity = request.form['quantity']
+        status = request.form['status']
+        created_by = g.user['id']
+        created_at = now
+        updated_by = g.user['id']
+        updated_at = now
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO course'
+                ' (title, description, image_url, price, quantity, status'
+                ', created_by, created_at, updated_by, updated_at)'
+                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                (title, description, image_url, price, quantity, status,
+                 created_by, created_at, updated_by, updated_at)
+            )
+            db.commit()
+            return redirect(url_for('course.index'))
+
+    return render_template('course/create.html')
